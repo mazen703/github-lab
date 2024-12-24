da8c6a8147ee4e90d7c99e4c6cf4a23226bf32f6
Mazen elbialey <mazenelbialy1010@gmail.com>
2024-12-24 11:35:42 +0200

    First commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Welcome to your first Node.js App!');
+});
+
+app.get('/about', (req, res) => {
+    res.send('This is a simple Node.js application for GitHub practice.');
+});
+
+const port = 3000;
+app.listen(port, () => {
+    console.log(`App running at http://localhost:${port}`);
+});
